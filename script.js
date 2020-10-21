516c1e73d1afa589c4839a23eaf2bf64b419865f
Timothy Brown <tabrownies@gmail.com>
2020-10-20 20:07:01 -0600

    Finsihed Tutorials, changed how the date displayed

---
+++ script.js
@@ -0,0 +1,100 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        number: '',
+        max: '',
+        current: {
+            title: '',
+            img: '',
+            alt: ''
+        },
+        loading: true,
+        addedName: '',
+        addedComment: '',
+        comments: {},
+    },
+    created() {
+        this.xkcd();
+    },
+    computed: {
+        month() {
+            var month = new Array;
+            if (this.current.month === undefined)
+                return '';
+            month[0] = "January";
+            month[1] = "February";
+            month[2] = "March";
+            month[3] = "April";
+            month[4] = "May";
+            month[5] = "June";
+            month[6] = "July";
+            month[7] = "August";
+            month[8] = "September";
+            month[9] = "October";
+            month[10] = "November";
+            month[11] = "December";
+            return month[this.current.month - 1];
+        }
+    },
+    watch: {
+        number(value, oldvalue) {
+            if (oldvalue === '') {
+                max = value;
+            } else {
+                this.xkcd();
+            }
+
+        },
+    },
+    methods: {
+        async xkcd() {
+            try {
+                this.loading = true;
+                let url = 'https://xkcd.now.sh/?comic=';
+                if (this.number === '') {
+                    url += 'latest';
+                } else {
+                    url += this.number;
+                }
+                const responce = await axios.get(url)
+                this.current = responce.data;
+                this.loading = false;
+                this.number = responce.data.num;
+
+
+            } catch (error) {
+                this.number = this.max;
+                console.log(error);
+            }
+
+        },
+        previousComic() {
+            this.number = this.current.num - 1;
+            if (this.number < 1)
+                this.number = 1;
+        },
+        nextComic() {
+            this.number = this.current.num + 1;
+            if (this.number > this.max)
+                this.number = this.max
+        },
+        getRandom(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum and minimum are inclusive
+        },
+        randomComic() {
+            this.number = this.getRandom(1, this.max);
+        },
+        addComment() {
+            if (!(this.number in this.comments))
+              Vue.set(app.comments, this.number, new Array);
+            this.comments[this.number].push({
+              author: this.addedName,
+              text: this.addedComment
+            });
+            this.addedName = '';
+            this.addedComment = '';
+          },
+    }
+});
